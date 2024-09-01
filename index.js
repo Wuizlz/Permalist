be471228fa54277bd65be74c6454f94ecead120b
Daniel <Brisenodan12@gmail.com>
2024-09-01 11:37:37 -0500

    Update CSS for modern to-do list style with background animation

--- index.js
+++ index.js
@@ -16,31 +16,34 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = []
-db.query("SELECT * FROM items ", (err, res) =>
-{
-  if(err)  console.error("Error executing query", err.stack);
+let items = [];
+db.query("SELECT * FROM items ", (err, res) => {
+  if (err) console.error("Error executing query", err.stack);
   items = res.rows;
-
-})
-
-app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    listTitle: "Today",
-    listItems: items,
-  });
 });
 
-app.post("/add", async (req, res) => {  
+app.get("/", async (req, res) => {
+  try {
+    const result = db.query("SELECT * FROM items ORDER BY id ASC ");
+    items = (await result).rows;
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listItems: items,
+    });
+  } catch {
+    console.log(err);
+  }
+});
+
+app.post("/add", async (req, res) => {
   const item = req.body.newItem;
   try {
-  const result = await db.query("INSERT INTO items (title) VALUES ($1) RETURNING *",
-    [item]
-  )
-  items.push(result.rows[0])
-  res.redirect("/");
-}
-  catch (err) {
+    const result = await db.query(
+      "INSERT INTO items (title) VALUES ($1) RETURNING *",
+      [item]
+    );
+    res.redirect("/");
+  } catch (err) {
     console.error("Error inserting item into the database", err.stack);
     res.status(500).send("Internal Server Error");
   }
@@ -49,29 +52,29 @@ app.post("/add", async (req, res) => {
 app.post("/edit", async (req, res) => {
   const editId = req.body.updatedItemId;
   const newTitle = req.body.updatedItemTitle;
-  try{
+  try {
     console.log(editId);
     console.log(newTitle);
-    const result = await db.query("UPDATE items SET title = $1 WHERE id = $2 RETURNING *",
-      [newTitle,editId]
-    )
-
-    const index = items.findIndex((item) => item.id === parseInt(editId));
-    if(index !== -1)
-    {
-      items[index].title = newTitle
-    }
+    const result = await db.query(
+      "UPDATE items SET title = $1 WHERE id = $2 RETURNING *",
+      [newTitle, editId]
+    );
     res.redirect("/");
-    
-
-  }
-  catch(err)
-  {
+  } catch (err) {
     console.error("Error inserting item into the database", err.stack);
     res.status(500).send("Internal Server Error");
   }
 });
-app.post("/delete", (req, res) => {});
+app.post("/delete", async (req, res) => {
+  const id = req.body.deleteItemId;
+  try {
+    console.log(id);
+    const result = await db.query("DELETE FROM items WHERE id = $1", [id]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
